36256506c5bc55460948b7792803f24f50abf255
Nathalie B <37987956+nathbrow42@users.noreply.github.com>
2021-12-06 17:14:03 -0500

    validation and stat update for form input

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
+  let list = document.getElementById("faultyItems");
+  list.style.visibility = "hidden";
+  let form = document.querySelector("form");
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+  form.addEventListener("submit", function () {
+    event.preventDefault();
+    let pilotInput = document.querySelector('[name="pilotName"]');
+    let pilot = pilotInput.value;
+    let copilotInput = document.querySelector('[name="copilotName"]');
+    let copilot = copilotInput.value;
+    let fuelLevelInput = document.querySelector("[name='fuelLevel']");
+    let fuelLevel = fuelLevelInput.value;
+    let cargoMassInput = document.querySelector("[name='cargoMass']");
+    let cargoMass = cargoMassInput.value;
+
+    formSubmission(document, list, pilot, copilot, fuelLevel, cargoMass);
+  });
+
+  let listedPlanets;
+  // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+  let listedPlanetsResponse;
+  listedPlanetsResponse
+    .then(function (result) {
+      listedPlanets = result;
+      console.log(listedPlanets);
+    })
+    .then(function () {
+      console.log(listedPlanets);
+      // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+    });
 });
